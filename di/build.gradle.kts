8904f991f8eb1e0585239bf6b45b258a16bd6171
Android No.1 <yeon8747@naver.com>
2023-02-11 08:12:50 +0900

    🗂 :: Hilt 의존성 추가

---
+++ di/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id(BuildPlugins.ANDROID_LIBRARY_PLUGIN)
+    id(BuildPlugins.KOTLIN_ANDROID_PLUGIN)
+    id(BuildPlugins.DAGGER_HILT_PLUGIN)
+    id(BuildPlugins.KOTLIN_KAPT)
+}
+
+android {
+    namespace = ProjectProperties.NAME_SPACE_DI
+    compileSdk = ProjectProperties.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = ProjectProperties.COMPILE_SDK
+        targetSdk = ProjectProperties.COMPILE_SDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = ProjectProperties.JVM_VERSION
+    }
+}
+
+dependencies {
+
+    implementation(project(":domain"))
+    implementation(project(":data"))
+
+
+    implementation(Dependency.HILT.HILT)
+    kapt(Dependency.HILT.HILT_COMPILER)
+
+
+    implementation(Dependency.RETROFIT.RETROFIT_CLIENT)
+    implementation(Dependency.RETROFIT.GSON_CONVERTER)
+
+    implementation(Dependency.Android.CORE_KTX)
+    implementation(Dependency.Android.APP_COMPAT)
+    implementation(Dependency.Android.MATERIAL)
+    testImplementation(Dependency.TEST.JUNIT)
+    androidTestImplementation(Dependency.TEST.EXT_JUNIT)
+    androidTestImplementation(Dependency.TEST.ESPRESSO)
+
+}
