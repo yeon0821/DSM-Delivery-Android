9a62f80e38ee5480ee2834a5efbff1575ee38cd2
Android No.1 <yeon8747@naver.com>
2023-02-17 19:45:52 +0900

    🗂 ::	rename package data

--- data/src/androidTest/java/com/delivery/data/ExampleInstrumentedTest.kt
+++ data/src/androidTest/java/com/delivery/data/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.data
+package com.delivery.data
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- data/src/test/java/com/delivery/data/ExampleUnitTest.kt
+++ data/src/test/java/com/delivery/data/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.data
+package com.delivery.data
 
 import org.junit.Test
 
